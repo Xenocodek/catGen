463bb836d0161711b5aa7dc9dc7e445a2ace5fb6
Xenocode <maxikote2012@gmail.com>
2023-09-23 16:32:02 +0300

    change run file

--- run.py
+++ run.py
@@ -8,6 +8,11 @@ from app.settings.config import Config
 TOKEN = Config.BOT_TOKEN
 
 async def start():
+    """
+    Configure logging, create a bot instance, initialize a dispatcher,
+    include the router, start polling, and close the bot session.
+    """
+    # Configure logging
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -18,15 +23,20 @@ async def start():
             ]
     )
 
+    # Create a bot instance
     bot = Bot(TOKEN, parse_mode='HTML')
 
+    # Initialize a dispatcher
     dp = Dispatcher()
 
+    # Include the router
     dp.include_router(router)
 
     try:
+        # Start polling
         await dp.start_polling(bot)
     finally:
+        # Close the bot session
         await bot.session.close()
 
 if __name__ == '__main__':
